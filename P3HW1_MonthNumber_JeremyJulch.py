380c0310b75650404eae559101a02398a404e31b
jjulch <55770190+jjulch@users.noreply.github.com>
2019-10-04 16:24:23 -0400

    Add files via upload

---
+++ P3HW1_MonthNumber_JeremyJulch.py
@@ -0,0 +1,40 @@
+# CTI-110
+# P3HW1 - Month number
+# Jeremy Julch
+# 3OCT2019
+#
+
+
+
+
+# Input the month number.
+MonthNum = int(input('Enter the month number 1 thur 12: '))
+
+# Determine the month from the number input.
+if MonthNum == 1:
+    print('The Month is January.')
+elif MonthNum == 2:
+    print('The Month if Feburary.')
+elif MonthNum == 3:
+    print('The Month is March.')
+elif MonthNum == 4:
+    print('The Month is April.')
+elif MonthNum == 5:
+    print('The Month is May.')
+elif MonthNum == 6:
+    print('The Month is June.')
+elif MonthNum == 7:
+    print('The Month is July.')
+elif MonthNum == 8:
+    print('The Month is August.')
+elif MonthNum == 9:
+    print('The Month is September.')
+elif MonthNum == 10:
+    print('The Month is October.')
+elif MonthNum == 11:
+    print('The Month is November.')
+elif MonthNum == 12:
+    print('The Month is December.')
+else:
+    print('Error please enter number from 1 to 12')
+
---
+++ P3LAB_Debugging_JeremyJulch.py
@@ -0,0 +1,22 @@
+#CTI-110
+#P3LAB-Debugging
+#Jeremy Julch
+#3OCT2019
+#
+
+
+# Setting up if elif else statements with proper indentation.
+if score >= A_score:
+    print('Your grade is A.')
+else:
+     if score >= B_score:
+        print('Your grade is B.')
+else:
+     if score >= C_score:
+        print('Your grade is C.)
+else:
+     if score >= D_score:
+        print('Your score is D.')
+else:
+     if score >= F_score:
+        print('Your score is F.')
